dc082a0f1864cd79b0cfc4c5a45cf3b3bd15bd48
Mudi Ogra <ograabiodun@gmail.com>
2020-02-09 07:45:25 +0100

    server.js change

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const session = require("client-sessions");
+const PORT = 1234;
+const app = express();
+
+// Retrieve
+var db
+var p
+var MongoClient = require('mongodb').MongoClient; 
+MongoClient.connect('mongodb+srv://user:password1234@cluster0-pjbde.mongodb.net/test?retryWrites=true&w=majority', function(err, db) {
+  if(!err) {
+  p = db
+    console.log("Connection successful");
+}
+  else
+  {
+  db = client.db('MuDB')
+  }
+});
