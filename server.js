1be8c13e4b096721fd00f58a3a74eebcccb5d0ae
Mudi Ogra <ograabiodun@gmail.com>
2020-02-09 07:53:12 +0100

    server.js change3

--- server.js
+++ server.js
@@ -18,6 +18,47 @@ MongoClient.connect('mongodb+srv://user:password1234@cluster0-pjbde.mongodb.net/
   }
 });
 
+
+//get Login
+app.use(session({
+    cookieName: 'session',
+    secret: 'random_string_goes_here',
+    duration: 30 * 60 * 100,
+    activeDuration: 5 * 60 * 1000,
+  })
+  );
+
+//login page/ default page
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+  
+  });
+
+  //log in as users or Admin
+app.get("/showallrecord", (req, res) => {
+
+    const dbo = p.db("MuDB");
+    
+    dbo.collection('courses').find().toArray(function(err, results) {
+     
+    if(results)
+      {
+    
+      console.log(results.toArray)
+    
+    // to see the first element
+      res.send(results)
+    
+      }
+    
+    else
+     console.log(err)
+    
+      // send HTML file populated with quotes here
+    })
+    
+    });
+
 // SHOW LOG THAT NODE SERVER STARTED
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`);
